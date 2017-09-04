ca73aa4524f0ac0c192c9ecb1ff3dfee7f00b3df
shiroemons <shiroemons@gmail.com>
2017-09-05 00:55:04 +0900

    Setup application.rb

--- template.rb
+++ template.rb
@@ -61,3 +61,30 @@ rails_command 'webpacker:install'
 
 # Generate secret key
 run 'bundle exec rails secrets:setup'
+
+# config/application.rb
+application do
+  %q{
+    config.time_zone = 'Tokyo'
+
+    I18n.enforce_available_locales = true
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    config.i18n.default_locale = :ja
+
+    config.generators do |g|
+      g.orm :active_record
+      g.template_engine :haml
+      g.test_framework :rspec, fixture: true
+      g.fixture_replacement :factory_girl, dir: 'spec/factories'
+      g.view_specs false
+      g.controller_specs true
+      g.routing_specs false
+      g.helper_specs false
+      g.request_specs false
+      g.assets false
+      g.helper false
+    end
+
+    config.paths.add 'lib', eager_load: true
+  }
+end
